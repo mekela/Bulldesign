8141902bccf19d766817a91b893a731d01495731
alex <sania_free@i.ua>
2016-07-06 16:37:15 +0300

    all pages

--- js/common.js
+++ js/common.js
@@ -11,26 +11,43 @@ $(document).ready(function() {
 	//show menu
 	$( ".menu_item" ).click(function() {
 	  $( ".fixed_menu" ).toggleClass( "active" );
+	  $( ".fixed_menu_overlay" ).toggleClass( "active" );
 	});
 	$( ".hamburger" ).click(function() {
 	  $( '.hamburger' ).toggleClass( "is-active" );
 	});
+	$( ".fixed_menu_overlay" ).click(function() {
+	  $( ".fixed_menu" ).toggleClass( "active" );
+	  $( ".fixed_menu_overlay" ).toggleClass( "active" );
+	  $( '.hamburger' ).toggleClass( "is-active" );
+	});
 
 	//load
-	$( ".load_page_wrap" ).delay( 15000 ).fadeOut( 400 );
+	$( ".load_page_wrap" ).delay( 8000 ).fadeOut( 400 );
 
 	$(".load_text").typed({
         // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
         stringsElement: $('.typed-strings'),
-        typeSpeed: 30,
+        typeSpeed: 7,
         backDelay: 500,
         loop: false,
         contentType: 'html', // or text
         // defaults to false for infinite loop
-        loopCount: false,
-        callback: function(){ foo(); },
-        resetCallback: function() { newTyped(); }
+        loopCount: false
+        
     });
+
 });
 
 new WOW().init();
+
+$(window).scroll(function(){
+  var threshold = 10; 
+  var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / threshold;
+	if( op <= 0 ){
+		$(".load_page_wrap").hide(); 
+	} else {
+		$(".load_page_wrap").hide();
+	}
+	
+});
